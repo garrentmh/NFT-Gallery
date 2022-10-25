deac6f1a10df147cfe7d18028e696c89036bce6e
garrentmh <garrentmh@gmail.com>
2022-10-25 09:28:28 +0800

    Add copy button for address

--- nft-demo-rtw3/pages/components/nftCard.jsx
+++ nft-demo-rtw3/pages/components/nftCard.jsx
@@ -10,6 +10,7 @@ export const NFTCard = ({nft}) => {
                 <h2 className="text-xl text-gray-800">{nft.title}</h2>
                 <p className="text-gray-600">Id: {nft.id.tokenId.substr(nft.id.tokenId.length - 4)}</p>
                 <p className="text-gray-600" >{`${nft.contract.address.substr(0, 4)}...${nft.contract.address.substr(nft.contract.address.length - 4)}`}</p>
+                <button onClick={() => {navigator.clipboard.writeText(nft.contract.address);}}>Copy</button>
             </div>
             <div className="flex-grow mt-2">
                 <p className="text-gray-600">{nft.description?.substr(0, 150)}</p>
